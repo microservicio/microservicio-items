d203dae7f4c71f7f2ae100cbf6325cf32b925dd0
Diego213073 <ma213073@gmail.com>
2023-04-22 17:59:45 -0500

    descartango parametro url en la anotación feign

--- src/main/java/com/app/items/infraestructure/clientRest/ProductServiceRest.java
+++ src/main/java/com/app/items/infraestructure/clientRest/ProductServiceRest.java
@@ -5,11 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "${client.rest.application.name}", url = "${client.rest.url}")
+@FeignClient(name = "${client.rest.application.name}")
 public interface ProductServiceRest {
 
     @GetMapping("api-product")
