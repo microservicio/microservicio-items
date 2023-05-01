4f53d5aa347f8c0943053922b70821850b0fcaca
Diego213073 <ma213073@gmail.com>
2023-05-01 16:48:03 -0500

    agregando configuración config client para apuntar al servicio de configuración conig-server para acceder a los datos del application.yml

--- src/main/java/com/app/items/infraestructure/api/ItemController.java
+++ src/main/java/com/app/items/infraestructure/api/ItemController.java
@@ -2,22 +2,40 @@ package com.app.items.infraestructure.api;
 
 import com.app.items.domain.models.Item;
 import com.app.items.domain.ports.ItemServicePort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("api-items")
 public class ItemController {
 
     private final ItemServicePort itemServicePort;
 
+    @Value("${configuracion.texto}")
+    private String texto;
+
     public ItemController(ItemServicePort itemServicePort) {
         this.itemServicePort = itemServicePort;
+    }
+
+    @GetMapping(value = "obtener-configuracion/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> getConfiguration(@Value("$[server.port}") String port) {
+        log.info(texto);
+        var response = new HashMap<String, String>();
+        response.put("texto", texto);
+        response.put("puerto", port);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping
